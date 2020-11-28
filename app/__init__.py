522f1e83d4c9b179d2ab62d9a398da16a340d5e6
alexconerly <alexanderconerly@gmail.com>
2020-11-27 20:43:23 -0600

    adding basic template

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def hello():
     return "<h1>Hello World!</h1>"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
